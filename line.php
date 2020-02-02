213120ed95d1cd1e43fdcae3431510bf1a1055d0
spacewater07 <60560093+spacewater07@users.noreply.github.com>
2020-02-02 10:45:02 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = 'Q6YJ9PuETRL2Cw3xM5lA8Ae4TZFYEdOmkuPAbRew3SAI1f47lsJDwbZ4K1MrTLpDu4zukgWfa9B61nQHtv6w/YYjyPEPiOmiYL9vdiUFTitKtDWxOhdLYPRqaHUTRU68sDP4wVsmTwX1cxVKiMVLMAdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => '088onvrp',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
